d259d189bdd3ad948972ba5d89892f399d05da2b
git-marcopitra <marcomorete73@hotmail.com>
2024-03-19 20:38:50 +0100

    :sparkles: feat: restyle header

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { A, Div, H1, Img, Nav } from '@stylin.js/elements';
+import { Div, H1, Img } from '@stylin.js/elements';
 import { FC } from 'react';
 
 const Navbar: FC = () => (
@@ -14,11 +14,6 @@ const Navbar: FC = () => (
     <H1 title="The Book of Andlew Tote" aria-label="The Book of Andlew Tote">
       <Img height="5rem" src="/img/logo.webp" alt="The Book of Andlew Tote" />
     </H1>
-    <Nav display="flex" gap="3rem" fontSize="1.6rem">
-      <A href="https://chart.com">Chart</A>
-      <A href="https://telegram.com">Telegram</A>
-      <A href="https://x.com">Twitter</A>
-    </Nav>
   </Div>
 );
 
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -5,37 +5,81 @@ import { Footer, Layout, Navbar } from '@/components';
 
 const Home: FC = () => (
   <Layout>
-    <Header bg="rgb(255, 225, 194)">
+    <Header bg="rgb(255, 225, 194)" position="relative">
       <Navbar />
       <Div
         my="5rem"
         mx="auto"
         gap="5rem"
-        display="flex"
+        display="grid"
+        maxWidth="1024px"
         alignItems="center"
-        flexDirection="column"
+        justifyItems="center"
+        gridTemplateColumns={['1fr', '1fr 1fr']}
       >
         <Img
           width="15rem"
           height="15rem"
           objectFit="cover"
           borderRadius="50%"
-          src="/img/book.webp"
+          src="/img/andlew-tote.webp"
           alt="The Book of Andlew Tote with Background"
         />
-        <Button
-          all="unset"
-          py="1rem"
-          px="2rem"
-          fontSize="1.5rem"
-          borderRadius="2rem"
-          bg="rgb(1, 209, 214)"
-          fontFamily="GochiHand"
-          boxShadow="3px 3px 1px #fff"
+        <Div
+          p="2rem"
+          gap="2rem"
+          display="flex"
+          flexDirection="column"
+          alignItems={['center', 'start']}
         >
-          Buy Now
-        </Button>
+          <H2 fontSize="3rem">Andlew Tote</H2>
+          <P fontSize="1.8rem">The book with the best Andlew Tote Quotes</P>
+          <Button
+            all="unset"
+            py="1rem"
+            px="2rem"
+            fontSize="1.5rem"
+            borderRadius="2rem"
+            bg="rgb(1, 209, 214)"
+            fontFamily="GochiHand"
+            boxShadow="3px 3px 1px #fff"
+          >
+            Buy Now
+          </Button>
+        </Div>
       </Div>
+      <Img
+        top="15%"
+        left="6%"
+        width="10rem"
+        position="absolute"
+        alt="You are F broke"
+        src="/img/youre-f-broke.webp"
+      />
+      <Img
+        top="6%"
+        right="3%"
+        width="10rem"
+        position="absolute"
+        alt="Winners never quit"
+        src="/img/winners-never-quit.webp"
+      />
+      <Img
+        bottom="0%"
+        left="10%"
+        width="10rem"
+        position="absolute"
+        alt="Winners never quit"
+        src="/img/what-color-is-your-bugatti.webp"
+      />
+      <Img
+        right="10%"
+        bottom="10%"
+        width="10rem"
+        position="absolute"
+        alt="Winners never quit"
+        src="/img/youre-broke.webp"
+      />
     </Header>
     <Main flex="1" p="5rem">
       <Div
